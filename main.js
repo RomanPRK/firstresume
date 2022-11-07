d9f28dfec3f035f28869397997a6e4bfc3be4f49
RomanPRK <117646595+RomanPRK@users.noreply.github.com>
2022-11-07 16:20:33 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+let button = document.querySelector("button");
+button.addEventListener("click", function(){
+    let c = document.querySelector('aside');
+    c.classList.toggle('hidden');
+    
+    let b = document.querySelector('main');
+    b.classList.toggle('hidden');
+    
+    if(b.classList.contains('hidden')){
+         button.innerHTML = "Показать опыт работы";
+    } else {
+        button.innerHTML = "Показать дополнительную информацию";
+
+    }
+})
+
+
+
